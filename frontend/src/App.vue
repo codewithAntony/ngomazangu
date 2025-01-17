206d92563624a3a7f628940c23e4fea2006aea84
Antony Murithi <antonymurithi51@gmail.com>
2025-01-17 19:08:26 +0300

    added images to my homepage . Also added links to signup page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,86 +1,71 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header class="bg-[#FBE9F3]">
+        <nav class="max-w-5xl mx-auto min-h-screen flex flex-col pt-4">
+            <div class="flex justify-between">
+                <div class="flex">
+                    <router-link to="/">
+                        <span
+                            class="text-black font-bold text-1xl sm:text-3xl uppercase"
+                            >ngoma</span
+                        ><span
+                            class="text-[#FECA11] font-bold text-1xl sm:text-3xl uppercase"
+                            >Zangu</span
+                        >
+                    </router-link>
+                </div>
+                <div class="flex gap-3">
+                    <router-link to="/signup">
+                        <button
+                            class="bg-black text-white px-11 py-2 rounded text-sm w-fit mt-2 hover:bg-[#FECA11] hover:text-black"
+                        >
+                            SignUp
+                        </button>
+                    </router-link>
+                </div>
+            </div>
+            <div class="flex-1 flex items-center justify-center">
+                <div
+                    class="flex flex-row gap-8 items-center max-w-4xl mx-auto px-4"
+                >
+                    <div class="flex flex-col space-y-2">
+                        <h2
+                            class="text-[#4095C3] font-extrabold text-2xl pb-3 sm:text-4xl"
+                        >
+                            Discover your music streaming stats
+                        </h2>
+                        <p class="text-[#4095C3] font-medium text-1xl">
+                            Get insights to your music listening habits with
+                            <br />
+                            top artists and tracks. Discover new music, create
+                            <br />
+                            playlists and get monthly reports.
+                        </p>
+                        <router-link to="/signup">
+                            <button
+                                class="bg-black text-white px-11 py-2 rounded text-sm w-fit mt-2 hover:bg-[#FECA11] hover:text-black"
+                            >
+                                SignUp
+                            </button>
+                        </router-link>
+                    </div>
+                    <div class="">
+                        <img
+                            src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2xnNDZ4bHA2ZDYzbnRraXlrYTZ4NHk4eGd4b3A0dHNvbDVxa2JxdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5tiNlHkA1WdUh3jRDW/giphy.gif"
+                            alt="Dancing Penguin"
+                            class="rounded-full"
+                            width="90%"
+                        />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-      <h1 class="text-red-500">hello</h1>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style></style>
