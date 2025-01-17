e8d860463de872dc33a2851a9dfea15e64accbf0
Antony Murithi <antonymurithi51@gmail.com>
2025-01-18 01:13:12 +0300

    created the sidebar for the dashboard component

---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,66 @@
+<script lang="ts"></script>
+<template>
+    <header class="p-4">
+        <div class="flex flex-col space-y-6">
+            <div class="">
+                <RouterLink to="/" class="inline-block">
+                    <span
+                        class="text-black font-normal text-2xl sm:text-3xl sm:font-bold uppercase"
+                        >ngoma</span
+                    >
+                    <span
+                        class="text-[#FECA11] font-normal text-2xl sm:text-3xl sm:font-bold uppercase"
+                        >Zangu</span
+                    >
+                </RouterLink>
+            </div>
+            <nav>
+                <ul class="flex flex-col space-y-4">
+                    <li>
+                        <a
+                            href="#spotify"
+                            class="text-gray-700 hover:text-black"
+                            >Spotify</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            href="#dashboard"
+                            class="text-gray-700 hover:text-black"
+                            >Dashboard</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            href="#top-artists"
+                            class="text-gray-700 hover:text-black"
+                            >Top Artists</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            href="#top-tracks"
+                            class="text-gray-700 hover:text-black"
+                            >Top Tracks</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            href="#recently-played"
+                            class="text-gray-700 hover:text-black"
+                            >Recently Played</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            href="#new-discovery"
+                            class="text-gray-700 hover:text-black"
+                            >New Discovery</a
+                        >
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+<script></script>
